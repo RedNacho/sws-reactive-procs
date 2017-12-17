d89c80aae30c84fbb2ff0c31323305f2fdd60aa9
Simon White <simon.white@commercetools.de>
2017-12-17 18:54:45 +0100

    Refactoring RS test to use Scala Async (makes it a bit clearer).

--- src/test/scala/sws/reactiveprocs/reactivestreams/ReactiveProcsPublisherTest.scala
+++ src/test/scala/sws/reactiveprocs/reactivestreams/ReactiveProcsPublisherTest.scala
@@ -1,11 +1,14 @@
 package sws.reactiveprocs.reactivestreams
 
+import java.util.concurrent.atomic.AtomicLong
+
 import org.reactivestreams.Publisher
 import org.reactivestreams.tck.{PublisherVerification, TestEnvironment}
 import sws.reactiveprocs.ReactiveProcs.Done
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.async.Async._
 
 /**
   * Created by simonwhite on 12/16/17.
@@ -14,16 +17,16 @@ class ReactiveProcsPublisherTest extends PublisherVerification[Long](new TestEnv
 
   override def createPublisher(elements: Long): Publisher[Long] = {
     ReactiveProcsPublisher[Long](
-      algorithmFactory = () => (yieldReturn: (Long) => Future[Done.type], _: () => Future[Done.type]) => {
-        def yieldAll(sent: Long, remaining: Long): Future[Done.type] = {
-          remaining match {
-            case 0 => Future.successful(Done)
-            case _ =>
-              yieldReturn(sent).flatMap(_ => yieldAll(sent + 1, remaining - 1))
-          }
+      algorithmFactory = () => (yieldReturn: (Long) => Future[Done.type], _: () => Future[Done.type]) => async {
+        @volatile
+        var sent: Long = 0
+
+        while (sent < elements) {
+          await { yieldReturn(sent) }
+          sent += 1
         }
 
-        yieldAll(0, elements)
+        Done
       },
       lookAhead = 10)
   }
